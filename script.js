37adb440c5f5c4842ee487297c464579bad09cff
xxy130330 <yangxue0708@gmail.com>
2017-11-07 23:33:17 -0800

    created two divs in display area, append clicked button to div#expression

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var inputElementArr = [];
 
-
-
-
-
 $(document).ready(initiateApp);
 
 function initiateApp() {
@@ -16,43 +12,77 @@ function initiateApp() {
     handleAllInput(inputElementArr);
 }
 
-function display(){
-
-}
 
 function handleAllInput(arr) {
-    for(var i=0; i< arr.length; i++){
-        if(!isNaN(arr[i])){
-
+    for(var i=0; i<arr.length; i++){
+        if(!isNaN(arr[i]) && !isNaN(arr[i+1])){
+            arr[i] = '' + arr[i] + arr[i+1];
+            arr.splice(i+1,1);
+            i--;
         }
     }
 }
 
 function handleNumInput() {
     var numInput = $(this).text();
+    $('#expression').append(numInput);
     inputElementArr.push(numInput);
+    handleAllInput(inputElementArr);
 }
 
 function handleOperatorInput() {
     var opInput = $(this).text();
-    inputElementArr.push(opInput);
+    $('#expression').append(opInput);
+    if(checkFirstElementAtInputArr()) {
+        inputElementArr.push(opInput);
+        handleAllInput(inputElementArr);
+    }
 }
 
 function handleDotInput() {
     var dot = $(this).text();
-
+    $('#expression').append(dot);
+    inputElementArr.push(dot);
+    handleAllInput(inputElementArr);
 }
 
 function handleEqualInput() {
     var equalSign = $(this).text();
-
+    if(checkFirstElementAtInputArr()) {
+        inputElementArr.push(equalSign);
+    }
 }
 
 function handleDeletePartial() {
-    var del = $(this).text();
-
+    $('#expression button').last().remove();
 }
 
 function handleDeleteAll() {
-    var deleteAll = $(this).text();
+    $('#expression').empty();
+    inputElementArr=[];
 }
+
+function checkFirstElementAtInputArr() {
+    if(inputElementArr[0] == null){
+        return true;
+    }
+}
+// var cal = new calculator(newFunc)
+// var val =$(this).click()
+//
+//
+// function newFunc(type, value, item) {
+//         $('#displayArea').text(value);
+// }
+//
+// $(document).ready(initiateApp);
+// function initiateApp() {
+//     $('.number').click(handleNumInput);
+//     $('.operator').click(handleOperatorInput);
+//     $('.dot').click(handleDotInput);
+//     $('.equal').click(handleEqualInput);
+//     $('.delete').click(handleDeletePartial);
+//     $('.deleteEverything').click(handleDeleteAll);
+//     var val = $(this).text();
+//     handleAllInput(inputElementArr);
+// }
